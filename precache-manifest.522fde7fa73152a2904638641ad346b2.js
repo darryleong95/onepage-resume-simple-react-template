8f74e75960606c5d2d05e6358aac934fb9feb985
Darryl Leong <darryleong95@gmail.com>
2019-09-05 22:51:02 +0800

    Updates

---
+++ precache-manifest.522fde7fa73152a2904638641ad346b2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30d274d228fffd322b2b46353bb40c9",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "e00866911ec4a1b8603c",
+    "url": "/repository-name/static/css/main.d51fd411.chunk.css"
+  },
+  {
+    "revision": "c36f94813127e0289ea0",
+    "url": "/repository-name/static/js/2.8ae53aba.chunk.js"
+  },
+  {
+    "revision": "e00866911ec4a1b8603c",
+    "url": "/repository-name/static/js/main.2c2fbbde.chunk.js"
+  },
+  {
+    "revision": "f4824e5187e8db0c5e19",
+    "url": "/repository-name/static/js/runtime~main.0afb898f.js"
+  },
+  {
+    "revision": "62a14559e15ef57fe5bf354b716e6e56",
+    "url": "/repository-name/static/media/ach1.62a14559.jpg"
+  },
+  {
+    "revision": "b9368c446a13fd3f88f719b83fdce5c2",
+    "url": "/repository-name/static/media/ach2.b9368c44.jpg"
+  },
+  {
+    "revision": "af5b61d9b07cbb9acd8db4506aabf4e5",
+    "url": "/repository-name/static/media/ach3.af5b61d9.png"
+  },
+  {
+    "revision": "a62dc6094e00ead9cd20a548d4fbc2b0",
+    "url": "/repository-name/static/media/badge1.a62dc609.png"
+  },
+  {
+    "revision": "26f84de2b0aa8fd0f97240f0f5069f53",
+    "url": "/repository-name/static/media/badge2.26f84de2.png"
+  },
+  {
+    "revision": "3afefe35870295f1bfec177836d3ed0a",
+    "url": "/repository-name/static/media/badge3.3afefe35.png"
+  },
+  {
+    "revision": "4b64dc8e7600d7aa14ccfbed1530ab09",
+    "url": "/repository-name/static/media/banner-ipad.4b64dc8e.jpg"
+  },
+  {
+    "revision": "6680cf278942bcd44acc360ea32c203a",
+    "url": "/repository-name/static/media/banner.6680cf27.png"
+  },
+  {
+    "revision": "2942bfabb3d05332b66eb128e0842cff",
+    "url": "/repository-name/static/media/dummycv.2942bfab.pdf"
+  },
+  {
+    "revision": "ebc996ba9fc7fa9a9506d157b3eb228e",
+    "url": "/repository-name/static/media/education1.ebc996ba.jpg"
+  },
+  {
+    "revision": "5d59fe381c69f0f471efec4b39d82d66",
+    "url": "/repository-name/static/media/education2.5d59fe38.jpg"
+  },
+  {
+    "revision": "025b9e5cf959af8a7388abe50ea31b7d",
+    "url": "/repository-name/static/media/facebook.025b9e5c.png"
+  },
+  {
+    "revision": "b0de6006aee3fce314fc2575019d0b0d",
+    "url": "/repository-name/static/media/google.b0de6006.jpg"
+  },
+  {
+    "revision": "917a01527bab1deb23874f9bfb024e50",
+    "url": "/repository-name/static/media/involvement1.917a0152.png"
+  },
+  {
+    "revision": "28bdbe78f4c21aeaa1be3bfb829adb7c",
+    "url": "/repository-name/static/media/involvement2.28bdbe78.jpg"
+  },
+  {
+    "revision": "8a7eaf8c69fed6100430a9127f7c63d7",
+    "url": "/repository-name/static/media/involvement3.8a7eaf8c.jpg"
+  },
+  {
+    "revision": "2bd6b3f1afbb611df6f8f8740ec46375",
+    "url": "/repository-name/static/media/profile.2bd6b3f1.jpg"
+  },
+  {
+    "revision": "12c818700091888050eee322444b21ee",
+    "url": "/repository-name/static/media/profilemobile.12c81870.jpg"
+  },
+  {
+    "revision": "7e6508d8e087cdc6927d450ab0cbf92c",
+    "url": "/repository-name/static/media/skills.7e6508d8.png"
+  }
+]);
